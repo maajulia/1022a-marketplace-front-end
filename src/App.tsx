ec2a4014017f480b3a631bb2cd2b6ee25b9a8282
maajulia <118356114+maajulia@users.noreply.github.com>
2024-10-21 14:55:00 -0400

    adicionando o usuario

--- src/App.tsx
+++ src/App.tsx
@@ -7,39 +7,69 @@ type ProdutoType = {
     descricao:string,
     imagem:string
 }
+
+type UsuarioType = {
+  id: number,
+  nome: string,
+  email: string
+};
+
 function App() {
   const [nome, setNome] = useState("")
   const [produtos, setProdutos] = useState<ProdutoType []>([])
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([]);
+
+
   //useEffects(O que fazer, quando Fazer) []=> Hora do carregamento da página
   useEffect(() => {
-    setNome("Guilherme Terenciani")
+    setNome("Maria Julia")
     //Buscar os dados do BackENd
-    fetch("https://one022a-marketplace-e90o.onrender.com/produtos")
+    fetch("https://one022a-marketplace-924k.onrender.com/produtos")
       .then(resposta => resposta.json())
       .then(dados => setProdutos(dados))
     //Colocar em uma variável
-  }, [])
+
+
+
+   fetch("https://one022a-marketplace-924k.onrender.com/usuarios")
+  .then(resposta => resposta.json())
+  .then(dados => setUsuarios(dados))
+}, [])
+
+
 
   return (
     <>
-      <h1>{nome}</h1>
-      <div className="produtos-container">
-        {
-          produtos.map(produto => {
-            return (
-              <div key={produto.id} className="produto-item">
-                <h1>{produto.nome}</h1>
+    <h1>{nome}</h1>
+    <div className="produtos-container">
+      {
+        produtos.map(produto => {
+          return (
+            <div key={produto.id} className="produto-item">
+              <h1>{produto.nome}</h1>
+              <div className='container-imagem'>
                 <img src={produto.imagem} alt="Imagem do celular" />
-                <p>{produto.preco}</p>
-                <p>{produto.descricao}</p>
               </div>
-            )
-          })
-        }
+              <p>{produto.preco}</p>
+              <p>{produto.descricao}</p>
+            </div>
+          )
+        })
+      }
 
+    </div>
+    <h2>Usuários</h2>
+      <div className="usuarios-container">
+        {usuarios.map(usuario => (
+          <div key={usuario.id} className="usuario-item">
+            <h3>{usuario.nome}</h3>
+            <p>Email: {usuario.email}</p>
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
+ 
 
 export default App
